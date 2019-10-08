40d33a76e8ea7b31b614211b2363447b951e74c7
YiyinEllenGu <yiyingu0512@gmail.com>
2019-10-08 10:11:14 -0400

    update

--- ARportal/Assets/Scripts/PortalCamera.cs
+++ ARportal/Assets/Scripts/PortalCamera.cs
@@ -6,13 +6,22 @@ public class PortalCamera : MonoBehaviour
 {
 
     //public Transform portal;
-
+    private Vector3 begin_pos;
+    private Vector3 begin_rot;
     // Update is called once per frame
     public Transform ARcamera;
     // Update is called once per frame
     void Update()
     {
-            transform.position = new Vector3(100 - 3 * ARcamera.position.x, (float)0.3 +3 * ARcamera.position.y, (float)-1.8+ 3 * ARcamera.position.z);
-            transform.rotation = Quaternion.Euler(new Vector3(ARcamera.rotation.eulerAngles.x, -ARcamera.rotation.eulerAngles.y, -ARcamera.rotation.eulerAngles.z) );
+        if (GameObject.FindWithTag("plane") != null)
+        {
+            transform.position = new Vector3(100 - 3 * (ARcamera.position.x-begin_pos.x), 2 + 3 * (ARcamera.position.y-begin_pos.y), -6 + 3 * (ARcamera.position.z-begin_pos.z));
+            transform.rotation = Quaternion.Euler(new Vector3(ARcamera.rotation.eulerAngles.x - begin_rot.x, -(ARcamera.rotation.eulerAngles.y - begin_rot.y), 0));
+        }
+        else
+        {
+            begin_pos = ARcamera.position;
+            begin_rot = ARcamera.rotation.eulerAngles;
+        }
     }
 }
